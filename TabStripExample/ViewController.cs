4d098756b535cab9467fd3e20b57e5d168393b18
Drew Frisk <dfrisk@redarrowlabs.com>
2017-06-19 18:59:58 -0500

    updated example projec
    * check the right titles so the expected background colors get set for child view controllers
    * removed “Reveal.app” from the mtouch arguments. project doesn’t build unless you have reveal installed

--- TabStripExample/ViewController.cs
+++ TabStripExample/ViewController.cs
@@ -9,138 +9,136 @@ using CoreGraphics;
 
 namespace TabStripExample
 {
-    [Register("UniversalView")]
-    public class UniversalView : UIView
-    {
-        public UniversalView()
-        {
-            Initialize();
-        }
+	[Register("UniversalView")]
+	public class UniversalView : UIView
+	{
+		public UniversalView()
+		{
+			Initialize();
+		}
 
-        public UniversalView(CGRect bounds) : base(bounds)
-        {
-            Initialize();
-        }
+		public UniversalView(CGRect bounds) : base(bounds)
+		{
+			Initialize();
+		}
 
-        void Initialize()
-        {
-            BackgroundColor = UIColor.White;
-        }
-    }
+		void Initialize()
+		{
+			BackgroundColor = UIColor.White;
+		}
+	}
 
-    [Register("ViewController")]
-    public class ViewController : ButtonBarPagerTabStripViewController/*, IPagerTabStripDataSource*/
-    {
-        UIColor themeColor = UIColor.FromRGB(165, 16, 129);
-        public ViewController()
-        {
+	[Register("ViewController")]
+	public class ViewController : ButtonBarPagerTabStripViewController/*, IPagerTabStripDataSource*/
+	{
+		UIColor themeColor = UIColor.FromRGB(165, 16, 129);
+		public ViewController()
+		{
 
-        }
+		}
 
-        public ViewController(IntPtr handle)
-            : base(handle)
-        {
-        }
+		public ViewController(IntPtr handle)
+			: base(handle)
+		{
+		}
 
-        public ViewController(NSCoder coder) : base(coder)
-        {
+		public ViewController(NSCoder coder) : base(coder)
+		{
 
-        }
+		}
 
-        public ViewController(string nibName, NSBundle bundle) : base(nibName, bundle)
-        {
+		public ViewController(string nibName, NSBundle bundle) : base(nibName, bundle)
+		{
 
-        }
+		}
 
-        public override void DidReceiveMemoryWarning()
-        {
-            // Releases the view if it doesn't have a superview.
-            base.DidReceiveMemoryWarning();
+		public override void DidReceiveMemoryWarning()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning();
 
-            // Release any cached data, images, etc that aren't in use.
-        }
+			// Release any cached data, images, etc that aren't in use.
+		}
 
-        public override void ViewDidLoad()
-        {
-            View = new UniversalView(View.Frame);
-            View.BackgroundColor = UIColor.FromRGB(214,214,214);
+		public override void ViewDidLoad()
+		{
+			View = new UniversalView(View.Frame);
+			View.BackgroundColor = UIColor.FromRGB(214, 214, 214);
 
-            Settings.Style.ButtonBarBackgroundColor = themeColor;
-            Settings.Style.ButtonBarItemBackgroundColor = themeColor;
-            Settings.Style.SelectedBarBackgroundColor = UIColor.White;
-            Settings.Style.ButtonBarItemFont = UIFont.BoldSystemFontOfSize(12);
-            Settings.Style.SelectedBarHeight = 4;
-            Settings.Style.ButtonBarMinimumLineSpacing = 0;
-            Settings.Style.ButtonBarItemTitleColor = UIColor.White;
-            Settings.Style.ButtonBarItemsShouldFillAvailiableWidth = true;
-            Settings.Style.ButtonBarLeftContentInset = 0;
-            Settings.Style.ButtonBarRightContentInset = 0;
-            Settings.Style.ButtonBarHeight = 48;
+			Settings.Style.ButtonBarBackgroundColor = themeColor;
+			Settings.Style.ButtonBarItemBackgroundColor = themeColor;
+			Settings.Style.SelectedBarBackgroundColor = UIColor.White;
+			Settings.Style.ButtonBarItemFont = UIFont.BoldSystemFontOfSize(12);
+			Settings.Style.SelectedBarHeight = 4;
+			Settings.Style.ButtonBarMinimumLineSpacing = 0;
+			Settings.Style.ButtonBarItemTitleColor = UIColor.White;
+			Settings.Style.ButtonBarItemsShouldFillAvailiableWidth = true;
+			Settings.Style.ButtonBarLeftContentInset = 0;
+			Settings.Style.ButtonBarRightContentInset = 0;
+			Settings.Style.ButtonBarHeight = 48;
 
-            ChangeCurrentIndexProgressive = changeCurrentIndexProgressive;
+			ChangeCurrentIndexProgressive = changeCurrentIndexProgressive;
 
-            base.ViewDidLoad();
+			base.ViewDidLoad();
 
-            // Perform any additional setup after loading the view
-        }
+			// Perform any additional setup after loading the view
+		}
 
-        public override UIViewController[] CreateViewControllersForPagerTabStrip(PagerTabStripViewController pagerTabStripViewController)
-        {
-            ChildViewController controller1 = new ChildViewController("PODCASTS");
-            ChildViewController controller2 = new ChildViewController("VIDEOS");
-            ChildViewController controller3 = new ChildViewController("TRENDING");
-            ChildViewController controller4 = new ChildViewController("PROFILE");
-            //ChildViewController controller5 = new ChildViewController("Mithra");
-            //ChildViewController controller6 = new ChildViewController("Varuna");
+		public override UIViewController[] CreateViewControllersForPagerTabStrip(PagerTabStripViewController pagerTabStripViewController)
+		{
+			ChildViewController controller1 = new ChildViewController("PODCASTS");
+			ChildViewController controller2 = new ChildViewController("VIDEOS");
+			ChildViewController controller3 = new ChildViewController("TRENDING");
+			ChildViewController controller4 = new ChildViewController("PROFILE");
 
-            return new UIViewController[] { controller1, controller2, controller3, controller4 };
-        }
+			return new UIViewController[] { controller1, controller2, controller3, controller4 };
+		}
 
-        void changeCurrentIndexProgressive(ButtonBarViewCell oldCell, ButtonBarViewCell newCell, nfloat progressPercentage, bool changeCurrentIndex, bool animated)
-        {
-            //if (changeCurrentIndex == true)
-            //{
-            //    if (oldCell != null)
-            //        oldCell.Label.TextColor = UIColor.White;
-            //    if (newCell != null)
-            //        newCell.Label.TextColor = crimson;
-            //}
-        }
-    }
+		void changeCurrentIndexProgressive(ButtonBarViewCell oldCell, ButtonBarViewCell newCell, nfloat progressPercentage, bool changeCurrentIndex, bool animated)
+		{
+			//if (changeCurrentIndex == true)
+			//{
+			//    if (oldCell != null)
+			//        oldCell.Label.TextColor = UIColor.White;
+			//    if (newCell != null)
+			//        newCell.Label.TextColor = crimson;
+			//}
+		}
+	}
 
-    public class ChildViewController : UIViewController, IIndicatorInfoProvider
-    {
-        public string Title { get; set; }
-        public ChildViewController(IntPtr handle) : base(handle) { }
-        public ChildViewController(string title)
-        {
-            Title = title;
-        }
-        public IndicatorInfo IndicatorInfoForPagerTabStrip(PagerTabStripViewController pagerTabStripController)
-        {
-            return new IndicatorInfo(Title);
-        }
+	public class ChildViewController : UIViewController, IIndicatorInfoProvider
+	{
+		public string Title { get; set; }
+		public ChildViewController(IntPtr handle) : base(handle) { }
+		public ChildViewController(string title)
+		{
+			Title = title;
+		}
+		public IndicatorInfo IndicatorInfoForPagerTabStrip(PagerTabStripViewController pagerTabStripController)
+		{
+			return new IndicatorInfo(Title);
+		}
 
-        public override void ViewDidLoad()
-        {
-            base.ViewDidLoad();
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
 
-            switch (Title)
-            {
-                case "STREAM":
-                    View.BackgroundColor = UIColor.White;
-                    break;
-                case "VIDEOS":
-                    View.BackgroundColor = UIColor.Blue;
-                    break;
-                case "TOPICS":
-                    View.BackgroundColor = UIColor.Orange; ;
-                    break;
-                case "SOURCES":
-                    View.BackgroundColor = UIColor.Green; ;
-                    break;
-            }
+			switch (Title)
+			{
+				case "PODCASTS":
+					View.BackgroundColor = UIColor.White;
+					break;
+				case "VIDEOS":
+					View.BackgroundColor = UIColor.Blue;
+					break;
+				case "TRENDING":
+					View.BackgroundColor = UIColor.Orange; ;
+					break;
+				case "PROFILE":
+					View.BackgroundColor = UIColor.Green; ;
+					break;
+			}
 
-        }
-    }
+		}
+	}
 }
